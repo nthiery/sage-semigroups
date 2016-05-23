41f06ebca0466980741c880929882ed991d72f72
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-05-23 12:27:58 +0200

    Added doc to the initialization module

--- sage_semigroups/__init__.py
+++ sage_semigroups/__init__.py
@@ -1,9 +1,13 @@
 """
-TESTS:
+Sage-Semigroups's initialization
 
-We test that the sage categories have been properly monkey patched.
+Loading this module initializes the library by monkey patching its
+features into the Sage library::
 
     sage: import sage_semigroups
+
+We test that the sage categories have been properly monkey patched::
+
     sage: Semigroups().Finite().parent_class.is_r_trivial
     <unbound method FiniteSemigroups.parent_class.is_r_trivial>
     sage: Semigroups().Finite().parent_class.is_r_trivial.__module__
@@ -12,5 +16,6 @@ We test that the sage categories have been properly monkey patched.
 from recursive_monkey_patch import monkey_patch
 import sage_semigroups
 import sage
+# TODO: do we want this? Should this list the features that are being patched?
 print "Loading sage-semigroups and patching its features into Sage's library"
 monkey_patch(sage_semigroups, sage)
