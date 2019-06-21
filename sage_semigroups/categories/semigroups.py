4b06a6694224899bca5b739db697a9c9adb4bd63
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-06-21 13:15:32 +0200

    Minimization

--- sage_semigroups/categories/semigroups.py
+++
@@ -1,8 +0,0 @@
-from sage.misc.cachefunc import cached_method
-
-class Semigroups:
-    class ParentMethods:
-
-        @cached_method
-        def cayley_graph_cached(self, *args, **kwds):
-            return self.cayley_graph(*args, **kwds)
